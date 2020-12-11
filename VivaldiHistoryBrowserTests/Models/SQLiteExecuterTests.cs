4029d0e5d2d05690ae8354b98414164c057c121a
Akn <sibata486@gmail.com>
2020-12-11 20:20:03 +0900

    クラス追加 / データベースへの接続と select を行うクラスを作成
    テストプロジェクトも同時に作成

---
+++ VivaldiHistoryBrowserTests/Models/SQLiteExecuterTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VivaldiHistoryBrowser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VivaldiHistoryBrowser.Models.Tests {
+    [TestClass()]
+    public class SQLiteExecuterTests {
+        [TestMethod()]
+        public void selectTest() {
+            var s = new SQLiteExecuter();
+            var list = s.select("select * from visits limit 10;");
+            Assert.Fail();
+        }
+    }
+}
