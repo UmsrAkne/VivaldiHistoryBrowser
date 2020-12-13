d2383836170ace84dc1498916a0de9c33a5c855c
Akn <sibata486@gmail.com>
2020-12-13 20:58:59 +0900

    機能追加 / 訪問時刻を算出する処理を追加

--- VivaldiHistoryBrowser/Models/DBHelper.cs
+++ VivaldiHistoryBrowser/Models/DBHelper.cs
@@ -19,11 +19,19 @@ namespace VivaldiHistoryBrowser.Models {
 
             var pages = new List<WebPage>();
 
+            // 履歴の日時は 1601,1,1 からの経過マイクロ秒数で表されている。
+            // 更に 9時間を足しているのは（多分）タイムゾーンによるズレがあり、訪問時刻がピッタリ9時間ずれているため。
+            var startDateTime = new DateTime(1601, 1, 1).AddHours(9);
+
             hashs.ForEach(h => {
                 var p = new WebPage() {
                     PageTitle = (String)h["title"],
                     URL = (String)h["url["],
                 };
+
+                long microSecVisiTime = (long)h["visit_time"];
+                var elapsedTicks = new TimeSpan(microSecVisiTime * 10);
+                p.visitDateTime = startDateTime + elapsedTicks;
 
                 pages.Add(p);
             });
