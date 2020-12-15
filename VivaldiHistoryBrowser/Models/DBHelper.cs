3afa9cdd6eb352ad75e43b8bcd6573b7d11e2138
Akn <sibata486@gmail.com>
2020-12-15 15:15:41 +0900

    機能追加 / 入力した日時型を履歴ファイルの日時の数値に変換する機能を実装

--- VivaldiHistoryBrowser/Models/DBHelper.cs
+++ VivaldiHistoryBrowser/Models/DBHelper.cs
@@ -38,5 +38,21 @@ namespace VivaldiHistoryBrowser.Models {
 
             return pages;
         }
+
+        /// <summary>
+        /// 入力された日時を、chrome用履歴ファイルの日時を表す数値に変換します。
+        /// </summary>
+        /// <param name="dt">変換したい日時を入力します。
+        /// chromeの日時を表す数値は 1601年1月1日(９時)が基準となるため、それより前の日時は例外を投げます。</param>
+        /// <returns></returns>
+        private long toChromeHistoryDateTimeNumber(DateTime dt) {
+            var startDateTime = new DateTime(1601, 1, 1).AddHours(9);
+            var elapsedTimeSpan = dt - startDateTime;
+            if(elapsedTimeSpan.Ticks < 0) {
+                throw new ArgumentException("1601年1月1日以前の数値は無効です");
+            }
+
+            return elapsedTimeSpan.Ticks / 10;
+        }
     }
 }
