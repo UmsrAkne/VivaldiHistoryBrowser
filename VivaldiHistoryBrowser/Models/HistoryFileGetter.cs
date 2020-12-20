cfad34d96b827206e9a11278a3f5f07ae402dd16
Akn <sibata486@gmail.com>
2020-12-20 23:52:14 +0900

    クラス追加 / 履歴ファイル取得を行う静的メソッドを持つクラスを追加

---
+++ VivaldiHistoryBrowser/Models/HistoryFileGetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VivaldiHistoryBrowser.Models {
+    class HistoryFileGetter {
+
+        private HistoryFileGetter() { }
+
+        public static String HistoryFilePath() {
+            System.Diagnostics.Debug.WriteLine(File.Exists($@"C:\Users\{Environment.UserName}\AppData\Local\Vivaldi\User Data\Default\History"));
+            return $@"C:\Users\{Environment.UserName}\AppData\Local\Vivaldi\User Data\Default\History";
+        }
+    }
+}
